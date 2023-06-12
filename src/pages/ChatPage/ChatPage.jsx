110cdcbae21506c2a5682bc0af4122a1450214d5
SeungPyo Hong <83650872+Jaharim@users.noreply.github.com>
2023-06-12 17:34:09 +0900

    Dir: 각 페이지별 폴더 및 파일 생성 (#4)
    - ChatPage
    - HelpPage
    - LoginPage
    - MyPage
    - NotFoundPage
    - PostPage
    - PreventPage
    - SignUpPage
    - SplashPage

---
+++ src/pages/ChatPage/ChatPage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ChatPage() {
+  return <div>ChatPage</div>;
+}
---
+++ src/pages/PreventPage/PreventPage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function PreventPage() {
+  return <div>PreventPage</div>;
+}
