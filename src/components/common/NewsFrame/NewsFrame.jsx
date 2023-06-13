34c171203c3427f28832c8ba9c54021cde61979e
SeungPyo Hong <83650872+Jaharim@users.noreply.github.com>
2023-06-13 16:17:20 +0900

    Dir: 공통 컴포넌트 폴더 및 jsx 파일 생성 (#9)
    - NavBar
    - NewsFrame
    - Slider

---
+++ src/components/common/NewsFrame/NewsFrame.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NewsFrame() {
+  return <div>NewsFrame</div>;
+}
---
+++ src/components/common/Slider/Slider.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Slider() {
+  return <div>Slider</div>;
+}
