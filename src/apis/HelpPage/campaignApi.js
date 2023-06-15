3a28da12d3d2609a26c1b58fc7cdbbdb9f0b6157
SeungPyo Hong <83650872+Jaharim@users.noreply.github.com>
2023-06-15 10:18:47 +0900

    Chore: 후원 및 캠페인 데이터 api 생성 (#13)

---
+++ src/apis/HelpPage/campaignApi.js
@@ -0,0 +1,40 @@
+const campaignData = [
+  {
+    endline: 16,
+    location: '강원도 강릉',
+    description: '화재 피해 복구',
+    participantGoal: 100,
+    participant: 32,
+    category: 'campaign',
+    type: 'wildfire',
+  },
+  {
+    endline: 37,
+    location: '제주도',
+    description: '호우 피해 복구',
+    participantGoal: 100,
+    participant: 57,
+    category: 'campaign',
+    type: 'flood',
+  },
+  {
+    endline: 5,
+    location: '경남 통영',
+    description: '낙후지역 벽화 그리기',
+    participantGoal: 20,
+    participant: 18,
+    category: 'campaign',
+    type: 'painting',
+  },
+  {
+    endline: 23,
+    location: '서울시 성북구',
+    description: '연탄 나르기',
+    participantGoal: 50,
+    participant: 41,
+    category: 'campaign',
+    type: 'briquette',
+  },
+];
+
+export default campaignData;
