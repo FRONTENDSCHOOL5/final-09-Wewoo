359795e8ca67080532b23e0ef5950ce2e6aaea4b
mango <user@mangoui-MacBookPro.local>
2023-06-18 23:15:43 +0900

    Dir,Refactor: 로그인, 회원가입 페이지 폴더구조 변경 및 공통 스타일, 아이콘 재사용

---
+++ src/pages/LoginAndSignUpPage/BackIcon.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const BackIcon = () => {
+  return (
+    <svg width='10' height='17' viewBox='0 0 10 17' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M9 16L1 8.5L9 0.999999'
+        stroke='#333333'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+
+export default BackIcon;
