a7411ddd7439aac44cfa503a89061e091ebc02dd
hyunhee <nnhhnn9592@gmail.com>
2023-06-20 18:39:22 +0900

    Feat: PreventPage 관련 Router 기능 추가 (#18)

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage/MainPage';
 import HelpPage from './HelpPage/HelpPage';
+import PreventPage from './PreventPage/PreventPage';
+import Checking from './PreventPage/components/Checking';
+import ActionTips from './PreventPage/components/ActionTips';
+import EmergencySupplies from './PreventPage/components/EmergencySupplies';
 
 export default function AppRouter() {
   return (
@@ -9,6 +13,10 @@ export default function AppRouter() {
       <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='/help' element={<HelpPage />} />
+        <Route path='/prevent' element={<PreventPage />} />
+        <Route path='/emergencysupplies' element={<EmergencySupplies />} />
+        <Route path='/checking' element={<Checking />} />
+        <Route path='/action' element={<ActionTips />} />
       </Routes>
     </BrowserRouter>
   );
