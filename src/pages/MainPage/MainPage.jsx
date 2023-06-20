c034a4c6e5c170cd510b9240a0709029733c2ee7
g1azed <victoria1230120@gmail.com>
2023-06-20 17:37:13 +0900

    Design: [ MainPage ] 공통컴포넌트 추가 및 코드 수정 (#6)

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
-import weatherImg from '../../assets/images/main/weatherEX.png';
-import profileImg from '../../assets/images/main/profileEX.png';
+
 import TopBar from '../../components/common/TopBar/TopBar';
 import SectionHeader from '../../components/common/SectionHeader/SectionHeader';
+import BottomNavBar from '../../components/common/BottomNavBar/BottomNavBar';
+
+// 예시로 넣은 이미지 임포트 (마지막에 빼야한다)
+import weatherImg from '../../assets/images/main/weatherEX.png';
+import profileImg from '../../assets/images/main/profileEX.png';
+
 const MainHeader = styled.section`
   width: 100%;
-  padding: 20px 20px 70px 20px;
+  padding: 70px 20px 70px 20px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   background-color: #191919;
+  position: relative;
 
-  div:first-child {
+  div {
     p.main-text {
       font-size: 24px;
       font-weight: 600;
@@ -37,7 +43,7 @@ const SlideUI = styled.ul`
   flex-direction: column;
   gap: 15px;
   width: 100%;
-  padding: 0 20px;
+  /* padding: 0 20px; */
   flex-wrap: nowrap;
   justify-content: initial;
   overflow-x: scroll;
@@ -161,12 +167,15 @@ const MainFollowList = styled.div`
   }
 `;
 export default function MainPage() {
+  // let navigate = useNavigate();
+
   return (
     <section className='container'>
       <div className='wrapper'>
         <h1 className='a11y-hidden'> 메인 페이지 </h1>
-        <TopBar />
+
         <MainHeader>
+          <TopBar />
           <div>
             <p className='main-text'>
               {' '}
@@ -198,20 +207,8 @@ export default function MainPage() {
               <span> 동안구 </span>
               <p> 미세먼지 없는 화창한 날씨예요</p>
             </SlideItem>
-            <SlideItem w='200' bgImg={weatherImg}>
-              <span> 동안구 </span>
-              <p> 미세먼지 없는 화창한 날씨예요</p>
-            </SlideItem>
           </div>
           <div>
-            <SlideItem w='200' bgImg={weatherImg}>
-              <span> 동안구 </span>
-              <p> 미세먼지 없는 화창한 날씨예요</p>
-            </SlideItem>
-            <SlideItem w='200' bgImg={weatherImg}>
-              <span> 동안구 </span>
-              <p> 미세먼지 없는 화창한 날씨예요</p>
-            </SlideItem>
             <SlideItem w='200' bgImg={weatherImg}>
               <span> 동안구 </span>
               <p> 미세먼지 없는 화창한 날씨예요</p>
@@ -290,6 +287,7 @@ export default function MainPage() {
             </div>
           </MainFollowList>
         </MainFollow>
+        <BottomNavBar />
       </div>
     </section>
   );
