598514f5ce9ec7e8f5a1aa3132639e79cd15e29b
SeungPyo Hong <83650872+Jaharim@users.noreply.github.com>
2023-06-24 20:05:08 +0900

    Feat: Router 구조 변경 (#37)
    - help 경로 안에 donation/:donationId 경로 추가로 중첩라우터 구성

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import MainPage from './MainPage/MainPage';
 import HelpPage from './HelpPage/HelpPage';
 import LoginPage from './LoginAndSignUpPage/LoginPage';
 import SignUpPage from './LoginAndSignUpPage/SignUpPage';
+import DonationPage from './DonationPage/DonationPage';
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<MainPage />} />
-        <Route path='/help' element={<HelpPage />} />
+        <Route path='/help/*' element={<Outlet />}>
+          <Route index element={<HelpPage />} />
+          <Route path='donation/:donationId' element={<DonationPage />} />
+        </Route>
         <Route path='/login' element={<LoginPage />} />
         <Route path='/sign-up' element={<SignUpPage />} />
       </Routes>
