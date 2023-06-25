e0446ff4391a1b4f525509464f834caeaf09a43a
anyangjin <ayz0155@gmail.com>
2023-06-26 00:26:23 +0900

    Feat: 계정id입력 후 엔터 시 다음으로 이동 기능구현, 이미있는 계정아이디일 경우의 에러 추가 (#12)

--- src/pages/LoginAndSignUpPage/SignUpPage/AccountNameForm.jsx
+++ src/pages/LoginAndSignUpPage/SignUpPage/AccountNameForm.jsx
@@ -6,25 +6,42 @@ export default function AccountNameForm({
   setErrorMessage,
   errorMessage,
   accountInfo,
+  handleNextButton,
 }) {
   const handleAccountName = (e) => {
     setAccountInfo((prev) => ({ ...prev, accountName: e.target.value }));
     if (errorMessage.includes('accountNameError')) {
       setErrorMessage((prev) => prev.filter((ele) => ele !== 'accountNameError'));
     }
+    if (errorMessage.includes('accountNameAlreadyError')) {
+      setErrorMessage((prev) => prev.filter((ele) => ele !== 'accountNameAlreadyError'));
+    }
   };
-
+  const handleInputEnter = (e) => {
+    if (e.keyCode === 13) {
+      handleNextButton();
+    }
+  };
   return (
     <>
       <StyledInputText>계정 ID</StyledInputText>
       <StyledInput
         value={accountInfo.accountName}
         onChange={handleAccountName}
-        className={errorMessage.includes('accountNameError') ? '' : 'mb-37'}
+        onKeyUp={handleInputEnter}
+        className={
+          errorMessage.includes('accountNameError') ||
+          errorMessage.includes('accountNameAlreadyError')
+            ? ''
+            : 'mb-37'
+        }
         placeholder='영문, 숫자, 특수문자(.), (_)만 사용 가능합니다.'
       />
       {errorMessage.includes('accountNameError') && (
         <StyledErrorMessage>*영문, 숫자, 밑줄, 마침표만 사용할 수 있습니다.</StyledErrorMessage>
+      )}
+      {errorMessage.includes('accountNameAlreadyError') && (
+        <StyledErrorMessage>*이미 가입된 계정ID 입니다.</StyledErrorMessage>
       )}
     </>
   );
